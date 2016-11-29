68d7257ae43cc810a084a4390324dabe82000138
Stefán Atli Ágústsson <stefanag261@nemi.tskoli.is>
2016-11-29 10:21:21 +0000

    ég er guð

--- php/post.user.selected.php
+++ php/post.user.selected.php
@@ -8,16 +8,19 @@ $ID = $data->userid;
 $toReturn = [];
 
 $tempuserarray = [];
+$tempcomparray = [];
 
 $keyword = "User";
+$keyword = "TeamComps"
 
 
 $sth = $dhb->prepare('SELECT 
-	id,
-	username,
-	name,
-	email,
-	imgLink FROM 
+	id AS ID,
+	username AS Username,
+	name AS Name,
+	email AS Email,
+	imgLink AS imgLink
+	FROM 
 	user WHERE 
 	id = :ID');
 
@@ -27,6 +30,26 @@ $sth->setFetchMode(PDO::FETCH_ASSOC);
 
 $tempuserarray = $sth->fetch();
 
+$sth = $dhb->prepare('SELECT	
+	t.id AS ID,
+	t.teamname AS TeamName,
+	ct.comp_id AS compID,
+	c.compDate AS compDate,
+	g.name AS gameName
+	from team t
+	inner join team_user tu on t.id = tu.team_id
+	inner join comp_team ct on tu.team_id = ct.teams_id
+	inner join comp c on ct.comp_id = c.id
+	inner join game g on c.games_id = g.id
+	WHERE tu.user_id = :ID
+	/*AND 
+	SELECT 
+	ct.comp_id AS compID
+	from comp_team ct 
+	inner join team t on ct.team_id = t.id
+	where ct.team_id = t.id*/');
+
+teams_id
 $toReturn[$keyword] = $tempuserarray;
 
 echo (json_encode($toReturn, JSON_UNESCAPED_SLASHES));
